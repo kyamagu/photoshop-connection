c6c4f968c7db6bc256c0ba32b2e4deeafea15893
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-22 11:19:52 +0900

    Update readthedocs dependency

--- docs/conf.py
+++ docs/conf.py
@@ -15,10 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 
